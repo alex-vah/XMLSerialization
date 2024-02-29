7286df6aa0b41064e4b4181a8924cdb41d8aebfe
alexvahanyan <alexvahanyan@gmail.com>
2024-02-29 17:33:44 +0400

    Initial commit

---
+++ XMLSerialization/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Xml.Serialization;
+
+namespace XMLSerialization
+{
+    public class Employee
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Department { get; set; }
+    }
+
+    [XmlRoot("Company")]
+    public class Company
+    {
+        [XmlArray("Employees")]
+        [XmlArrayItem("Employee")]
+        public List<Employee> Employees { get; set; }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Task 1: Deserialize XML
+            Company company;
+            using (var stream = new FileStream("samplexml.xml", FileMode.Open))
+            {
+                var serializer = new XmlSerializer(typeof(Company));
+                company = (Company)serializer.Deserialize(stream);
+            }
+
+            // Add a new employee
+            var newEmployee = new Employee
+            {
+                Id = 101,
+                Name = "John Doe",
+                Department = "HR"
+            };
+            company.Employees.Add(newEmployee);
+
+            // Serialize the new model
+            using (var stream = new FileStream("newxml.xml", FileMode.Create))
+            {
+                var serializer = new XmlSerializer(typeof(Company));
+                serializer.Serialize(stream, company);
+            }
+
+            Console.WriteLine("XML serialization and deserialization completed successfully!");
+        }
+    }
+}
